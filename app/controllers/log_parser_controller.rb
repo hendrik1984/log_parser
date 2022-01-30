d8d618aa8aa63bfb2727a0ed49ad28a2c9fb05ad
Hendrik Rahardja <hendrik@alterra.id>
2022-01-31 03:34:38 +0700

    refactory controller

--- app/controllers/log_parser_controller.rb
+++ app/controllers/log_parser_controller.rb
@@ -7,36 +7,7 @@ class LogParserController < ApplicationController
     @result_parses = []
 
     if @raw_data.file && params["search"] != nil
-      # TODO: create beautiful method path, this is ugly
-      path_blob = ActiveStorage::Blob.service.path_for(@raw_data.file.key)
-
-      # TODO create method to read file
-      apache_regex = /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - (.{0})- \[([^\]]+?)\] "(GET|POST|PUT|DELETE) ([^\s]+?) (HTTP\/1\.1)" (\d+) (\d+) ([^\s]+?) "(.*)"/
-
-      file = File.foreach(path_blob)
-      file.each_entry do |line|
-        parser = line.scan(apache_regex)[0]
-
-        if line.scan(apache_regex)[0].nil?
-          puts "Can't parser: #{line}\n\n"
-          next
-        end
-
-        parse = {
-          :ip         => parser[0],
-          :user       => parser[1],
-          :time       => parser[2],
-          :method     => parser[3],
-          :uri_path   => parser[4],
-          :protocol   => parser[5],
-          :code       => parser[6],
-          :res_size   => parser[7],
-          :referer    => parser[8],
-          :user_agent => parser[9]
-        }
-
-        @result_parses << parse if LogParserCustom.search(parse, params[:search])
-      end
+      @result_parses = LogParserCustom.parser(@raw_data, params["search"])
     else
       render status: :unprocessable_entity
     end
--- app/services/log_parser_custom.rb
+++ app/services/log_parser_custom.rb
@@ -1,5 +1,38 @@
 module LogParserCustom
     class << self
+
+        def parser(raw_data, params_search)
+            result_parses = []
+            path_blob = ActiveStorage::Blob.service.path_for(raw_data.file.key)
+            apache_regex = /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - (.{0})- \[([^\]]+?)\] "(GET|POST|PUT|DELETE) ([^\s]+?) (HTTP\/1\.1)" (\d+) (\d+) ([^\s]+?) "(.*)"/
+
+            file = File.foreach(path_blob)
+            file.each_entry do |line|
+                parser = line.scan(apache_regex)[0]
+
+                if line.scan(apache_regex)[0].nil?
+                    puts "Can't parser: #{line}\n\n"
+                    next
+                end
+
+                parse = {
+                    :ip         => parser[0],
+                    :user       => parser[1],
+                    :time       => parser[2],
+                    :method     => parser[3],
+                    :uri_path   => parser[4],
+                    :protocol   => parser[5],
+                    :code       => parser[6],
+                    :res_size   => parser[7],
+                    :referer    => parser[8],
+                    :user_agent => parser[9]
+                }
+
+                result_parses << parse if self.search(parse, params_search)
+            end
+            return result_parses
+        end
+
         def search(parse, params_search)
             valid = true
             params_search.each do |key, value|
@@ -15,3 +48,5 @@ module LogParserCustom
         end
     end
 end
+
+#TODO search by time must convert to integer to be more easier
