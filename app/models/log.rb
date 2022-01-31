6f5777060bbf1898f7e48e34ac0707b676acc0d3
Hendrik Rahardja <hendrik@alterra.id>
2022-01-31 21:38:24 +0700

    add validate name

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,7 +1,4 @@
 class Log < ApplicationRecord
     has_one_attached :file
-
-    # def log_file_path
-    #     ActiveStorage::Blob.service.path_for(file.key)
-    # end
+    validates :name, presence: true
 end
