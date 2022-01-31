b290078934aeef2e356ed2a6212d090fcef5e017
Hendrik Rahardja <hendrik@alterra.id>
2022-01-31 21:36:50 +0700

    refactor code

--- app/controllers/log_parser_controller.rb
+++ app/controllers/log_parser_controller.rb
@@ -2,16 +2,18 @@ class LogParserController < ApplicationController
   require 'log_parser_custom'
 
   def index
-    @raw_data = Log.last
-    @search = params["search"]
-    @result_parses = []
+    @raw_data       = Log.last
+    @search         = params["search"]
+    @result_parses  = []
 
-    if @raw_data
-      if @raw_data.file && params["search"] != nil
-        @result_parses = LogParserCustom.parser(@raw_data, params["search"])
-      else
-        render status: :unprocessable_entity
+    if @raw_data.present? && @raw_data.file && @search != nil
+      @validator = LogParserParams.new(@search)
+      if !@validator.valid?
+        redirect_to root_path, notice: "There is an error(s): #{@validator.errors.messages}"
       end
+
+      @result_parses = LogParserCustom.parser(@raw_data, @search)
     end
   end
+
 end
